dfbf4162dcf0d1dd156e4d00ef8cc4d891b69bb9
Yoann Tournebise <y.tournebise@groupeonepoint.com>
2017-03-13 16:21:12 +0100

    Ajout du partage de position de curseur.

--- src/client/scripts/main.js
+++ src/client/scripts/main.js
@@ -3,9 +3,47 @@
     socket     = io(),
     formEl     = document.getElementsByTagName('form')[0],
     inputEl    = document.getElementById('m'),
-    messagesEl = document.getElementById('messages')
+    messagesEl = document.getElementById('messages'),
+    cursors    = [{ x: 0, y: 0 }],
+    users      = {}
   ;
 
+
+  document.addEventListener('mousemove', function (event) {
+    cursors[0].x = event.pageX;
+    cursors[0].y = event.pageY;
+    socket.emit('mouse move', JSON.stringify(cursors[0]));
+  });
+
+
+  socket.on('user move', function (user) {
+    user = JSON.parse(user);
+
+    if (!users[user.id]) {
+      users[user.id] = user;
+
+      var userEl = document.createElement('div');
+      userEl.classList.add('user');
+      document.body.appendChild(userEl);
+      users[user.id].el = userEl;
+    }
+
+    users[user.id].el.style.left = user.x + 'px';
+    users[user.id].el.style.top  = user.y + 'px';
+  });
+
+  socket.on('user leave', function (user) {
+    user = JSON.parse(user);
+    document.body.removeChild(users[user.id].el);
+    delete users[user.id];
+  });
+
+
+
+  socket.on('chat message', function(msg){
+    messagesEl.innerHTML += '<li>' + msg + '</li>';
+    window.scrollTo(0, document.body.scrollHeight);
+  });
 
   formEl.onsubmit = function () {
     socket.emit('chat message', inputEl.value);
@@ -14,10 +52,4 @@
   };
 
   inputEl.focus();
-
-
-  socket.on('chat message', function(msg){
-    messagesEl.innerHTML += '<li>' + msg + '</li>';
-    window.scrollTo(0, document.body.scrollHeight);
-  });
 })();
--- src/server/main.js
+++ src/server/main.js
@@ -11,12 +11,31 @@ app.use(express.static(__dirname + '/../client/'));
 
 
 io.on('connection', function(socket){
-  socket.broadcast.emit('chat message', "Quelqu'un vous a rejoint.");
+  socket.broadcast.emit('chat message', `Quelqu'un a rejoint la salle.`);
+
+  socket.on('mouse move', function(mousePosition){
+    mousePosition = JSON.parse(mousePosition);
+    var user = {
+      id: socket.id,
+      x: mousePosition.x,
+      y: mousePosition.y
+    };
+    socket.broadcast.emit('user move', JSON.stringify(user));
+  });
 
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
+
+  socket.on('disconnect', function () {
+    var user = {
+      id: socket.id
+    };
+    socket.broadcast.emit('user leave', JSON.stringify(user));
+  });
 });
+
+
 
 
 
